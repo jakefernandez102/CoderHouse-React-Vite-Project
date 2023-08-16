ece75bcf895a7edfd509830f2ca1ec862d3dcd48
Jakesteve102 <jake.fernandezbrizuela@outlook.com>
2023-08-15 20:09:09 -0600

    adding routing

--- src/pages/ProductItem.jsx
+++ src/pages/ProductItem.jsx
@@ -16,15 +16,15 @@ const ProductItem = () => {
     }
 
     return (
-    <div className='flex flex-col lg:flex lg:flex-row p-10   items-center lg:h-full  gap-5'>
-        <div className='flex items-center justify-center bg-white shadow-lg p-10 cursor-pointer rounded-lg w-1/2 h-1/2'>
+    <div className='flex flex-col lg:flex lg:flex-row sm:p-10   items-center lg:h-full  gap-5'>
+        <div className='flex items-center justify-center bg-white shadow-lg p-10 cursor-pointer rounded-lg w-full h-full  md:w-1/2 md:h-1/2'>
             <div className=' flex items-center justify-center '>
                 <div>
                     <img className='block mx-auto' src={product.image} alt={`${title} image`} height={300} width={300} />
                 </div>
             </div>
         </div>
-        <div className='flex flex-col justify-between border-2 border-gray-400  w-1/2 h-1/2'>
+        <div className='flex flex-col justify-between border-2 border-gray-400 w-full h-full  md:w-1/2 md:h-1/2'>
             <div className='p-10 '>
                 <h2 className='boder border-b uppercase font-bold my-5 text-sm'>{product.title}</h2>
             </div>
@@ -38,10 +38,10 @@ const ProductItem = () => {
             <div className='flex justify-center'>
                 <Rate disabled allowHalf defaultValue={rating.rate} />
             </div>
-            <div className='flex gap-5 justify-center px-10 pb-5'>
+            <div className='flex gap-5 justify-center px-10 pb-5 '>
                 <button
                     type='button'
-                    className='shadow-lg mt-5 bg-cyan-200 px-5 py-3 rounded-lg font-bold uppercase hover:bg-cyan-400 transition-colors'
+                    className='text-sm shadow-lg mt-5 bg-cyan-200 px-5 py-3 rounded-lg font-bold uppercase hover:bg-cyan-400 transition-colors'
                     onClick={handleClick}
                     >
                     Add to car
@@ -49,7 +49,7 @@ const ProductItem = () => {
                 <Link
                     to='/'
                     type='button'
-                    className='shadow-lg mt-5 bg-gray-200 px-5 py-3 rounded-lg font-bold uppercase hover:bg-gray-400 transition-colors'
+                    className='text-sm shadow-lg mt-5 bg-gray-200 px-5 py-3 rounded-lg font-bold uppercase hover:bg-gray-400 transition-colors'
                     
                     >
                     Go back
